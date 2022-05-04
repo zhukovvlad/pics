f992f04ce6d7c451795e4d687a19d610dc6436ef
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-04 18:50:31 +0300

    Modified onSearchSubmit function into arrow function to prevent setState 'unknown function' error

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,25 @@ import SearchBar from "./SearchBar";
 import { unsplashKey } from '../keys';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: `Client-ID ${unsplashKey}`
-            }
-        });
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await axios
+            .get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: `Client-ID ${unsplashKey}`
+                }
+            });
+        
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
