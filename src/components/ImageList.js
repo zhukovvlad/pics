dae80c34d4cd5e87f6c0a249d3a21858004095a6
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-05 15:29:20 +0300

    Implemented initial CSS Grid System

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = props => {
-    const images = props.images.map((image) => {
-        return <img key={image.id} src={image.urls.regular} alt={image.description} />
+    const images = props.images.map(({id, urls, description}) => {
+        return <img key={id} src={urls.regular} alt={description} />
     });
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
