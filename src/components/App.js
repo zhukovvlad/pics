bfd1a5fa39e4891a05fec97aa6b64c2de9c24122
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-05 14:24:33 +0300

    Refactor App.js. Created separated 'api' folder where modified axios.create function is saved

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import axios from "axios";
 import SearchBar from "./SearchBar";
-import { unsplashKey } from '../keys';
+import unsplash from "../api/unsplash";
+
 
 class App extends React.Component {
     state = { images: [] };
 
     onSearchSubmit = async (term) => {
-        const response = await axios
-            .get('https://api.unsplash.com/search/photos', {
-                params: { query: term },
-                headers: {
-                    Authorization: `Client-ID ${unsplashKey}`
-                }
+        const response = await unsplash
+            .get('/search/photos', {
+                params: { query: term }
             });
         
         this.setState({ images: response.data.results });
